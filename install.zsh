7abe4f41a8e5d11a6ecc9f54db6fcf575e003cd5
Muhammad Qureshi <mutahirqureshi@gmail.com>
2017-04-08 01:08:53 -0700

    add install script

---
+++ install.zsh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+DIR="${0:A:h}"
+echo "DIR=$DIR"
+
+function create_link {
+  source="$1"
+  target="$2"
+
+  printf "linking %s to %s..." $source $target
+
+  if [ -e $target ]; then
+    printf "moving already existing %s to %s.bak..." $target $target
+    mv "$target" "${target}.bak"
+  fi
+
+  ln -s $source $target
+  printf "DONE!\n"
+}
+
+setopt EXTENDED_GLOB
+for rcfile in "$DIR"/runcoms/^README.md(.N); do
+  create_link "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
+
